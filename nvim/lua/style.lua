25b6a161fe29556a4b2136546e6650fa6424f517
JacobCrabill <jacob.crabill@gmail.com>
2024-03-13 08:42:19 -0700

    nvim: Update colorscheme config

--- nvim/lua/style.lua
+++ nvim/lua/style.lua
@@ -4,10 +4,22 @@ require('glow').setup({
   width = 150,
 })
 
+-- My custom color definitions
+my_colors = {
+  comment_pink = "#ee55a0",
+  comment_coral = "#d46398",
+  purple = "#C792EA",
+  purple2 = "#9C82D9",
+  dark_purple = "#B480D6",
+  coral = "#FF9CAC",
+  lime = "#BFD982",
+  baby_pink = "#D98293",
+}
+
 -- OneDark Color Scheme config
 require('onedark').setup({
   style = 'dark',
-  transparent = true, -- show/hide background
+  transparent = true,
 
   -- toggle theme style ---
   toggle_style_key = "<leader>ts",
@@ -20,9 +32,9 @@ require('onedark').setup({
   },
 
   colors = {
-    comment_pink = "#ee55a0",    -- My custom bright comment colors
-    comment_coral = "#d46398",
+    comment_pink = my_colors.comment_pink
   },
+
   highlights = {
     -- Treesitter token types
     ["@Comment"] = {fg = '$comment_pink'},
@@ -32,6 +44,69 @@ require('onedark').setup({
     ["@lsp.type.comment"] = { fg = '$comment_pink' },
   }
 })
-require('onedark').load()
+-- require('onedark').load()
+
+-- Tokyo Night style
+require("tokyonight").setup({
+  style = "storm", -- storm, moon, night, light
+  on_colors = function(colors)
+    colors.comment = my_colors.comment_coral
+  end
+})
+
+-- Material theme
+require("material").setup({
+    contrast = {
+      terminal = false, -- Enable contrast for the built-in terminal
+      sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+      floating_windows = true, -- Enable contrast for floating windows
+      cursor_line = false, -- Enable darker background for the cursor line
+      non_current_windows = false, -- Enable contrasted background for non-current windows
+      filetypes = {}, -- Specify which filetypes get the contrasted (darker) background
+  },
+
+  high_visibility = {
+      lighter = false, -- Enable higher contrast text for lighter style
+      darker = false, -- Enable higher contrast text for darker style
+  },
+
+  plugins = {
+    "nvim-tree",
+    "telescope",
+    "noice",
+    "nvim-cmp",
+  },
+
+  custom_colors = function(colors)
+      -- colors.syntax.comments = my_colors.coral
+      colors.syntax.comments = my_colors.baby_pink
+  end
+})
+
+-- Colorscheme Selection
+vim.g.my_scheme = "material"
+
+if vim.g.my_scheme == "material" then
+  vim.cmd('colorscheme material-palenight')
+
+elseif vim.g.my_scheme == "onedark" then
+  vim.cmd('colorscheme onedark')
+
+elseif vim.g.my_scheme == "nightfox" then
+  vim.cmd('colorscheme carbonfox')
+
+elseif vim.g.my_scheme == "palenight" then
+  vim.cmd('colorscheme palenight')
+  vim.g.palenight_terminal_italics = 1
+  -- Note: I think LSP highlighting is overriding this
+  vim.g.palenight_color_overrides = {
+    comment_grey = { gui = "#EE55E0", cterm = "213", cterm16 = "13" }
+  }
+
+elseif vim.g.my_scheme == "tokyonight" then
+  vim.cmd('colorscheme tokyonight')
+
+end
+
 
 -- vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
--- nvim/pretty.vim
+++ nvim/pretty.vim
@@ -1,34 +1,6 @@
 " ======== Colorscheme Config ========
-highlight Normal guibg=none ctermbg=none
-highlight NonText guibg=none ctermbg=none
+highlight Normal guibg=NONE ctermbg=NONE
 set background=dark
-
-let s:my_scheme = "onedark"
-
-if s:my_scheme == "nightfox"
-  " ---- Nightfox schemes
-  colorscheme carbonfox
-elseif s:my_scheme == "onedark"
-  " ---- OneDark scheme - Configured in Lua init
-  colorscheme onedark
-elseif s:my_scheme == "palenight"
-  " ---- Palenight scheme
-  colorscheme palenight
-  let g:palenight_terminal_italics=1
-  let g:palenight_color_overrides = {
-  \    'comment_grey': { "gui": "#EE55E0", "cterm": "213", "cterm16": "13" },
-  \}
-elseif s:my_scheme == "tokyonight"
-lua << EOF
-  require("tokyonight").setup({
-    style = "storm",
-    on_colors = function(colors)
-      colors.comment = "#d46390"
-    end
-  })
-EOF
-  colorscheme tokyonight-storm
-endif
 
 " " ==== Silicon Config (Render code to .png)
 " let g:silicon = {
